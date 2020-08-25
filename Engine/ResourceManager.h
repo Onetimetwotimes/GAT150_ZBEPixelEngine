9e113ec713c3998e3de290df696a3ccf3720147f
V. Daemond Antonio <DaeAnt@gmail.com>
2020-08-25 17:43:28 -0600

    ResourceManager reworked

--- Engine/ResourceManager.h
+++ Engine/ResourceManager.h
@@ -13,44 +13,21 @@ namespace zbe {
 
 		
 		template<typename T>
-		T* Get(const std::string name);
-		template<typename T>
-		bool Add(const std::string name, std::string filename, void* data = nullptr);
-
+		T* Get(const std::string name, void* data);
+	
 	protected:
-		std::map<std::string, Resource*> resources;
-
-		struct resource_not_found : public std::exception {
-			resource_not_found(std::string w) {
-				this->w = w.c_str();
-			}
-			const char* what() {
-				return w;
-			}
-		private:
-			const char* w;
-		};
+		std::map<std::string, zbe::Resource*> resources;
 	};
 
 	template<typename T>
-	inline T* ResourceManager::Get(const std::string name) {
+	inline T* ResourceManager::Get(const std::string name, void* data) {
 		T* source = dynamic_cast<T*>(resources[name]);
 
-		if (source == nullptr)
-			throw resource_not_found("No resource with name: " + name);
+		if (source == nullptr) {
+			source = new T;
+			source->Create(name, data);
+		}
 
 		return source;
-	}
-	template<typename T>
-	inline bool ResourceManager::Add(const std::string name, const std::string filename, void* data) {
-		if (resources[name] == nullptr) {
-			T* resource = new T;
-			if (!resource->Create(filename, data))
-				return false;
-			resources[name] = resource;
-			return true;
-		}
-		else
-			return false;
 	}
 }
